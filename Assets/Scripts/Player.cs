4e025793c52bb34f8fe4f7f17b98cdac8aa7c487
jedklika <43482414+jedklika@users.noreply.github.com>
2018-09-21 16:06:07 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    
+    public float speed;
+    private bool move=false;
+    public GameObject point;
+    private Vector3 target;
+
+    void Start() {
+       
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            if (move == false)
+                move = true;
+            Instantiate(point,target,Quaternion.identity);
+        }
+        if (move == true)
+            transform.position = Vector3.MoveTowards(transform.position,target,speed*Time.deltaTime);
+    }
+}
