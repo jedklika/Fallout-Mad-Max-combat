4d2d0ea3df40f6651fe134c9a0229958b63b7a75
Steven Stewart <stewart.steven98@outlook.com>
2018-09-25 22:11:04 -0400

    Some Overworld stuff
    Created most of the overworld, still need movement and LOTS of polish

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-    
+
     public float speed;
-    private bool move=false;
+    private bool move = false;
     public GameObject point;
     private Vector3 target;
+    public BoxCollider2D col;
 
-    void Start() {
-       
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+
     }
-    void Update() {
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             target.z = transform.position.z;
             if (move == false)
                 move = true;
-            Instantiate(point,target,Quaternion.identity);
+            Instantiate(point, target, Quaternion.identity);
         }
         if (move == true)
-            transform.position = Vector3.MoveTowards(transform.position,target,speed*Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("collider"))
+        {
+            SceneManager.LoadScene("Practice");
+        }
     }
 }
