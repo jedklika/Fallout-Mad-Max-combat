d65b93e1eea62b45c1c4fd843bb420503d915575
jedklika <43482414+jedklika@users.noreply.github.com>
2018-09-27 19:51:49 -0400

    Foes fight back
    no damage though

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,28 +8,55 @@ public class EnemyAI : MonoBehaviour {
 
     public float stoppingDistance;
 
+    public float retreatDistance;
+
     private Transform target;
 
     public int health;
 
-	// Use this for initialization
-	void Start () {
+    private float TimeBtwShot;
+
+    public float StartTimeBtwShot;
+
+    public GameObject Projectile;
+
+    // Use this for initialization
+    void Start () {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        TimeBtwShot = StartTimeBtwShot;
     }
     public void TakeDamage(int damage)
     {
         health -= damage;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if(Vector2.Distance(transform.position, target.position)> stoppingDistance)
+
+    // Update is called once per frame
+    void Update() {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, target.position) < stoppingDistance && (Vector2.Distance(transform.position, target.position) > retreatDistance))
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, target.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
         }
         if (health <= 0)
         {
             GameObject.Destroy(this.gameObject);
         }
-	}
-}
+        if (TimeBtwShot <= 0)
+        {
+            Instantiate(Projectile, transform.position, Quaternion.identity);
+            TimeBtwShot = StartTimeBtwShot;
+        }
+        else
+            {
+                TimeBtwShot -= Time.deltaTime;
+            }
+        
+    }
+    }
---
+++ Assets/Scripts/ProjectileFoe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFoe : MonoBehaviour {
+    public float speed;
+    public float lifeTime;
+    private Transform Player;
+    private Vector2 target;
+    public int damage;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(Player.position.x, Player.position.y);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Player>().TakeDamage(damage);
+            DestroyProjectile();       
+        }
+    }
+    void DestroyProjectile()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
+
