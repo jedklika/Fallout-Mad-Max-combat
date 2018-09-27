efbb42eeb4553ec9d4b3c44ab2695b226b979154
jedklika <43482414+jedklika@users.noreply.github.com>
2018-09-27 00:31:30 -0400

    Spawner
    spawning is back

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject Foe;
+    public bool StopSpawning = false;
+    public float SpawnTime;
+    public float SpawnDelay;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", SpawnTime, SpawnDelay);
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(Foe, transform.position, Quaternion.identity);
+        StopSpawning = true;
+        if (StopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
