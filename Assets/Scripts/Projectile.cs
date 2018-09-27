e3f7d765ed64e4251f75488a23f30f1cdef05337
jedklika <43482414+jedklika@users.noreply.github.com>
2018-09-27 01:30:00 -0400

    Combat
    turning in for the night, here what I got so far

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public float speed;
+    public float lifeTime;
+	// Use this for initialization
+	void Start () {
+        Invoke("DestroyProjectile", lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+	}
+    void DestroyProjectile()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+    }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooter : MonoBehaviour {
+public class Shooter : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject Projectile;
+    public Transform ShotPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+            if (Input.GetMouseButtonDown(1))
+            {
+                Instantiate(Projectile, ShotPoint.position, transform.rotation);
+        }
+    }
 }
